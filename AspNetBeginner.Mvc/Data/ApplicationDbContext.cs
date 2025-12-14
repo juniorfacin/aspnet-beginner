9a4f746f19d1a3aa4fbae8d98cace551328c0092
Junior Facin <jufacin@hotmail.com>
2025-12-14 13:14:25 -0300

    Add project files.

---
+++ AspNetBeginner.Mvc/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using AspNetBeginner.Mvc.Models;
+
+namespace AspNetBeginner.Mvc.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+    {
+        public DbSet<AspNetBeginner.Mvc.Models.Aluno> Aluno { get; set; } = default!;
+    }
+}
---
+++ AspNetBeginner.Mvc/Models/Aluno.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AspNetBeginner.Mvc.Models
+{
+    public class Aluno
+    {
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+        public string? Email { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime DataNascimento { get; set; }
+        public bool Ativo { get; set; }
+    }
+}
